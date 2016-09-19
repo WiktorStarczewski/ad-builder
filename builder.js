4cd36a2ddabb1efc0f96184d2223826909ce6bdd
Wiktor Starczewski <wiktor@squirro.com>
2016-09-19 16:27:27 +0200

    Bump version

--- builder.js
+++ builder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AD2460 Builder
 // @namespace    http://tampermonkey.net/
-// @version      0.3.3
+// @version      0.3.4
 // @description  try to take over the world!
 // @author       Anonymous
 // @match        http://live.ad2460.com/game.pl
